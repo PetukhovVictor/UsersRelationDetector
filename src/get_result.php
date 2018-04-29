591344ff8591e5f2928af010b9c5e36d63e3b458
Victor Petukhov <i@victor.am>
2018-04-29 19:00:45 +0300

    Add .idea files

--- src/get_result.php
+++ src/get_result.php
@@ -7,10 +7,4 @@ $memcacheD->flush();
 
 $data = $memcacheD->get("job_{$argv[1]}_result");
 
-//$memcacheD->delete("vk_queries_waiting_params");
-
-//print_r($memcacheD->getStats());
-
 print_r($data);
-
-file_put_contents('test.txt', gzcompress(json_encode($data)));
